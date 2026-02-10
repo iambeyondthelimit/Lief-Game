c3023803a4d4c97b82653204f24bf5e892551ddc
kaj <batistisjohnray@gmail.com>
2026-02-11 01:19:18 +0800

    Refactor Player class to simplify initialization
    Removed attack attribute from Player class and updated constructor.

--- player.py
+++ player.py
@@ -1,8 +1,15 @@
+import random
+
 class Player:
-    def __init__(self, name, hp, atk):
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        self.atk = atk.random.randint(10, 15)
-
+    
+    def attack(self):
+        return random.randint(10, 15)
+        
+    def heal(self):
+        return random.randint(10, 15)
+    
     def is_alive(self):
         return self.hp > 0
